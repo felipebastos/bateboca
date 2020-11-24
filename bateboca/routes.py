c1c17589f0ae07bad6ffa54b16b224aefa4d3bb4
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2020-11-24 15:01:08 -0300

    uso de formulários e request

--- bateboca/routes.py
+++ bateboca/routes.py
@@ -1,14 +1,27 @@
-from flask import current_app as app, render_template
+from flask import current_app as app, render_template, request
 
 @app.route('/')
 def inicio():
-    return render_template('filha.html')
+    return render_template('cad.html')
     
 @app.route('/dois')
 def dois():
     return render_template('filha2.html')
 
+
+@app.route('/cadastro', methods=['POST'])
+def cadastro():
+    nome_da_pessoa = request.form['nome']
+    senha = request.form['senha']
     
+    if nome_da_pessoa == 'felipebastos':
+        if senha == '123456':
+            return 'Login com sucesso'
+        else:
+            return 'Errou a senha'
+    else:
+        return 'Usuário inexistente'
+
     
     
     
