f8cf55c3e6580b523f6629681575461a05a23be7
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2020-11-26 12:55:04 -0300

    Corrigido para o script rodar com o python virtual por padrão, sem precisar ativar antes.

--- run_flask.sh
+++ run_flask.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-/bin/bash -c "source .venv/bin/activate"
+# Ativando o Python virtual do projeto
+. .venv/bin/activate
 
+# Preparando as variáveis do sistema para saber
+# qual flask rodar e em que modo.
+# "development" deixa o servidor rodando em
+# modo debug e etc.
 export FLASK_APP=bateboca
 export FLASK_ENV=development
 
