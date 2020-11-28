9b7b6bf68e2a70ae41f48ab2e7499288c1c9542b
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2020-11-28 00:36:41 -0300

    Agora o sistema cria a pasta de instância, inclusive o arquivo de banco de dados sqlite é criado dentro dela; o .gitignore está configurado para ignorar o conteúdo da pasta de instância, evitando assim vazamento de dados.

--- bateboca/__init__.py
+++ bateboca/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import click
+import os
 
 from flask.cli import with_appcontext
 
@@ -16,11 +17,16 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     app.secret_key = '123456'
     
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bateboca.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.instance_path,'bateboca.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
     
     db.init_app(app)
     
